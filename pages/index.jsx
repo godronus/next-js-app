93f242d950a0da1abc72fe30b494f42f23bd38dc
Gordon Farquharson <gordon.farquharson@gcore.com>
2024-11-05 10:27:02 +0000

    image added to app

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,12 @@
 export default function Index() {
-  return <div>Hello, World!</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Baja 2017</h1>
+      <img
+        src="/images/image.jpg"
+        alt="Centered Image"
+        className="w-auto max-h-[1000px]"
+      />
+    </div>
+  );
 }
